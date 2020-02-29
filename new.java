86cfa87bfac500d465c6ef20544b59534ca3aef9
Priyalblu <52493371+Priyalblu@users.noreply.github.com>
2020-02-29 15:49:45 +0530

    function added

--- new.java
+++ new.java
@@ -4,4 +4,11 @@ class new
   {
     System.out.println("New to GitHub!");
   }
+  void newFunc()
+  {
+    System.out.println("This is a new function and a new commit");
+    int a=10, b=5;
+    int c=a*b;
+    System.out.println("The multiplication is: "+c);
+  }
 }
